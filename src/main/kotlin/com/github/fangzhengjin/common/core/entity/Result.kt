d2b6110a5c0eb82fcf364c8204df7a3409231583
fangzhengjin <fangzhengjin@gmail.com>
2019-03-03 23:30:15 +0800

    Result去除Page强依赖，在common-jpa子组件中拓展

--- src/main/kotlin/com/github/fangzhengjin/common/core/entity/Result.kt
+++ src/main/kotlin/com/github/fangzhengjin/common/core/entity/Result.kt
@@ -2,9 +2,6 @@ package com.github.fangzhengjin.common.core.entity
 
 import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.serializer.SimplePropertyPreFilter
-import io.swagger.annotations.ApiModel
-import io.swagger.annotations.ApiModelProperty
-import org.springframework.data.domain.Page
 import org.springframework.http.HttpStatus
 import java.io.Serializable
 
@@ -23,30 +20,6 @@ class Result<T> @JvmOverloads constructor(
         var body: T? = "" as T
 ) : Serializable {
 
-    @ApiModel("分页数据集")
-    class PageResult<T>(
-            @ApiModelProperty("查询内容")
-            val content: List<T>,
-            @ApiModelProperty("总记录数")
-            val totalElements: Long,
-            @ApiModelProperty("总页数")
-            val totalPages: Int,
-            @ApiModelProperty("当前页码")
-            val currentPage: Int
-    )
-
-    @ApiModel("JQGrid分页数据集")
-    class JQGrid<T>(
-            @ApiModelProperty("查询内容")
-            val rows: List<T>,
-            @ApiModelProperty("总记录数")
-            val records: Long,
-            @ApiModelProperty("总页数")
-            val total: Int,
-            @ApiModelProperty("当前页码")
-            val page: Int
-    )
-
     val isOk: Boolean
         get() {
             return HttpStatus.OK.value() == this.code
@@ -57,27 +30,6 @@ class Result<T> @JvmOverloads constructor(
     }
 
     companion object {
-
-        @JvmStatic
-        fun <T> jqGrid(page: Page<T>): JQGrid<T> {
-            return JQGrid(
-                    rows = page.content,
-                    records = page.totalElements,
-                    total = page.totalPages,
-                    page = page.number + 1
-            )
-        }
-
-        @JvmStatic
-        fun <T> jqGrid(page: Page<T>, clazz: Class<T>, fields: List<String>): String {
-            return JSON.toJSONString(
-                JQGrid(
-                    rows = page.content,
-                    records = page.totalElements,
-                    total = page.totalPages,
-                    page = page.number + 1
-            ), SimplePropertyPreFilter(clazz, *fields.toTypedArray()))
-        }
 
         @JvmStatic
         @JvmOverloads
@@ -103,29 +55,14 @@ class Result<T> @JvmOverloads constructor(
         fun <T> ok(body: T?, clazz: Class<T>, fields: List<String>): String {
             return if (body != null) {
                 JSON.toJSONString(
-                    Result(
-                        code = HttpStatus.OK.value(),
-                        message = HttpStatus.OK.reasonPhrase,
-                        body = body
-                ), SimplePropertyPreFilter(clazz, *fields.toTypedArray()))
+                        Result(
+                                code = HttpStatus.OK.value(),
+                                message = HttpStatus.OK.reasonPhrase,
+                                body = body
+                        ), SimplePropertyPreFilter(clazz, *fields.toTypedArray()))
             } else {
                 ""
             }
-        }
-
-        @JvmStatic
-        @JvmOverloads
-        fun <T> ok(page: Page<T>, message: String? = null): Result<PageResult<T>> {
-            return Result(
-                    code = HttpStatus.OK.value(),
-                    message = message ?: HttpStatus.OK.reasonPhrase,
-                    body = PageResult(
-                            content = page.content,
-                            totalElements = page.totalElements,
-                            totalPages = page.totalPages,
-                            currentPage = page.number + 1
-                    )
-            )
         }
 
         @JvmStatic
